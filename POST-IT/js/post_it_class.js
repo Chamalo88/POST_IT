a9f9bb56eeddfaaa4faa509436946e9f1e1fdf16
Chamalo88 <80330845+Chamalo88@users.noreply.github.com>
2021-04-26 14:02:12 +0200

    Add files via upload

---
+++ POST-IT/js/post_it_class.js
@@ -0,0 +1,54 @@
+class post_it{
+texte;
+Height;
+Width ;
+couleur;
+x ;
+y
+
+
+ constructor (){
+    this.texte=("")
+    this.Height=120
+    this.Width=120
+    this.couleur="yellow"
+    this.x=150
+    this.y=150
+
+ }
+ 
+ 
+affiche() {
+   
+    let contener = document.getElementById('contener')
+
+  
+    let monElem = document.createElement('div');
+    monElem.classList.add("post_it")
+
+    monElem.style.texte=this.texte
+    monElem.style.top=this.y +"px"
+    monElem.style.left=this.x +"px"
+    monElem.style.height = this.Height+"px"
+    monElem.style.width = this.Width+"px"
+    monElem.style.backgroundColor=this.couleur
+  
+ 
+    contener.appendChild(monElem)
+}
+redim(L,H){
+                this.Width=L
+                this.Height=H
+
+}
+bouge(x,y){
+                this.x=x
+                this.y=y
+
+
+}
+
+couleurmodif(coul){
+                this.couleur=coul
+}
+}
---
+++ POST-IT/js/post_it_general.js
@@ -0,0 +1,12 @@
+Let postit1=new post_it ("postit1")
+
+postit1.affiche()
+
+postit1.bouge(400,300)
+postit1.affiche()
+
+postit1.coulormodif(pink)
+postit1.affiche()
+
+postit1.redim(190,120)
+postit1.affiche()
