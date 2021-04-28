dc3cf5547578b558cdc97562eec778bc7ef51b48
Chamalo88 <80330845+Chamalo88@users.noreply.github.com>
2021-04-28 15:05:13 +0200

    Add files via upload

--- POST-IT/js/post_it_general.js
+++ POST-IT/js/post_it_general.js
@@ -1,18 +1,70 @@
-let postit1=new post_it("postit1")
+let redim=false
+let bouge=false
+let inseretexte=false
+let sourisX
+let sourisY
+
+
+
+let postit1 =new post_it("postit1")
 
 postit1.affiche()
 
-postit1.bouge(400,300)
+
+document.body.addEventListener('click',()=>{
+redim=false
+bouge=false
+inseretexte=false
+
+})
+
+document.body.addEventListener('mousemove', (e)=>{
+
+sourisX=e.clientX
+sourisY=e.clientY
+
+
+if(bouge==true){
+postit1.bouge(e.clientX -20,e.clientY -20)
 postit1.affiche()
 
-postit1.couleurmodif("pink")
+}
+
+else if (redim==true){
+  
+postit1.redim(postit1.LInit +(e.clientX - postit1.sourisXInit),
+postit1.HInit+(e.clientY - postit1.sourisYInit))
 postit1.affiche()
 
-postit1.redim(190,220)
-postit1.affiche()
+}
 
-postit1.inseretexte("Bonjour, penser à moins penser la nuit!")
-postit1.affiche()
+})
+
+document.body.addEventListener('keydown',(e)=>{
+    if(inseretexte==true)
+if(e.key=='Enter'){
+    postit1.inseretexte(postit1.texte+"<br>")
+    postit1.affiche()
+}
+    else if(e.key=='Shift'){
+
+    }
+    else if (e.key==" "){
+        postit1.inseretexte(postit1.texte+"&nbsp;")
+        postit1.affiche()
+    
+
+    }
+    else if (e.key=="Backspace"){
+        postit1.inseretexte(postit1.texte.substr(0, postit1.texte.lenght -1))
+        postit1.affiche()
+    
+    }
 
 
+     else{ postit1.inseretexte(postit1.texte+e.key)
+        postit1.affiche()
+    
+
+}})
 
